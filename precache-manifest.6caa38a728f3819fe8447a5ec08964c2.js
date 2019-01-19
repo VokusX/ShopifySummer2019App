0739d87b051ea06fc72335e03a484008bba80a1b
VokusX <david@voicu.me>
2019-01-18 22:12:15 -0500

    Updates

---
+++ precache-manifest.6caa38a728f3819fe8447a5ec08964c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b813cf13f75eafe5fc",
+    "url": "/shopify/static/js/runtime~main.b8b813cf.js"
+  },
+  {
+    "revision": "4089a442e8c7f2ffe338",
+    "url": "/shopify/static/js/main.4089a442.chunk.js"
+  },
+  {
+    "revision": "107a4ec4179e846b914d",
+    "url": "/shopify/static/js/1.107a4ec4.chunk.js"
+  },
+  {
+    "revision": "4089a442e8c7f2ffe338",
+    "url": "/shopify/static/css/main.5bf604a1.chunk.css"
+  },
+  {
+    "revision": "95314b8d38ba51bba9dfafbdf3b3749d",
+    "url": "/shopify/index.html"
+  }
+];
